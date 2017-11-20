e7bdbbefcb539035b011b13ea46d958e8b60a1be
jaehanjung <wjdwogks23@gmail.com>
2017-11-20 16:55:48 +0900

    웹관련 용어 마크업 및 디자인 완성

---
+++ js/menu-tab1.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var menu = $('.main-menu > li');
+
+  menu.hover(function() {
+    $(this).find('.sub-menu').toggleClass('menu-act');
+  });
+  menu.focusin(function() {
+    $(this).siblings().find('.sub-menu').removeClass('menu-act');
+    $(this).find('.sub-menu').addClass('menu-act');
+  });
+
+});
