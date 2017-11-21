99af3d1a61f451642bf5f4c3cdffba4ccb538c75
jaehanjung <wjdwogks23@gmail.com>
2017-11-21 17:18:33 +0900

    검색및 공지사항/자료실 마크업 및 디자인 완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
   var menu = $('.main-menu > li');
+  var tab = $('.notice-heading, .pds-heading')
 
+  // 메인메뉴
   menu.hover(function() {
     $(this).find('.sub-menu').toggleClass('menu-act');
   });
   menu.focusin(function() {
     $(this).siblings().find('.sub-menu').removeClass('menu-act');
     $(this).find('.sub-menu').addClass('menu-act');
+  });
+
+  // 탭메뉴
+  tab.on('click focusin', function() {
+    $(this).parent().addClass('board-act').siblings().removeClass('board-act');
   });
 
 });
